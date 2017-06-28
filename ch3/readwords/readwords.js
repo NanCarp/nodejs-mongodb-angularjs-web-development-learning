16145666a8ccafa047323fc36588315b8bde3e58
NanCarp <nancarpli@163.com>
2017-06-28 10:36:14 +0800

    ch3 readme

--- ch3/readwords/readwords.js
+++ ch3/readwords/readwords.js
@@ -1,3 +1,4 @@
+// readwords.js：一个显示文本时加载 censorify 模块的 Node.js 应用程序
 var censor = require("censorify");
 console.log(censor.getCensoredWords());
 console.log(censor.censor("Some very sad, bad and mad text"));
