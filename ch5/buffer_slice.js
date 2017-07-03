91654fbc97559479802e2fe105a1539629da3091
NanCarp <nancarpli@163.com>
2017-07-03 15:26:33 +0800

    ch5

---
+++ ch5/buffer_slice.js
@@ -0,0 +1,7 @@
+var numbers = new Buffer("123456789");
+console.log(numbers.toString());
+var slice = numbers.slice(3, 6);
+console.log(slice.toString());
+slice[0] = '#'.charCodeAt(0);
+console.log(slice.toString());
+console.log(numbers.toString());
