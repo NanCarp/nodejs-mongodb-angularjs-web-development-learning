a3f56d598975695d1dfc32aaa482d2525c3ce2e1
NanCarp <nancarpli@163.com>
2017-07-11 15:48:30 +0800

    ch7

--- ch7/http_client_post.js
+++ ch7/http_client_post.js
@@ -1,22 +1,4 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    var jsonData = "";
-    req.on('data', function (chunk) {
-        jsonData += chunk;
-    });
-    req.on('end', function () {
-        var reqObj = JSON.parse(jsonData);
-        var resObj = {
-            message: "Hello " + reqObj.name,
-            question: "Are you a good " + reqObj.occupation + "?"
-        };
-        res.writeHead(200);
-        res.end(JSON.stringify(resObj));
-    });
-}).listen(8888);
-
-
-var http = require('http');
 var options = {
     host: '127.0.0.1',
     path: '/',
